09dc2d5d354b762bdae6355f44bcbe249c26ffab
Hovhannes <hovo141205@gmail.com>
2023-02-22 12:33:19 +0400

    First commit

---
+++ app/src/main/java/com/example/mazegame/MainActivity.java
@@ -0,0 +1,19 @@
+package com.example.mazegame;
+
+import androidx.annotation.Nullable;
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import android.util.AttributeSet;
+import android.view.View;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        /*GameView gameView = findViewById(R.id.view);*/
+    }
+}
